1f1a37f762756fbab06bb20a9b24aa66ed080af5
facudelucia <facundodeluciajudo@gmail.com>
2020-07-30 18:36:56 -0300

    calendar app v1.0

---
+++ public/precache-manifest.17cde12a414d3af26fd1e0ae3a5633ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5275b157dbf6218fc8c85083e7198845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7c8dace171d8c25fe7",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "004ec2cc00353a25035b",
+    "url": "/static/css/main.462fcb14.chunk.css"
+  },
+  {
+    "revision": "8e7c8dace171d8c25fe7",
+    "url": "/static/js/2.cb2bb3ed.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.cb2bb3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004ec2cc00353a25035b",
+    "url": "/static/js/main.bc58a030.chunk.js"
+  },
+  {
+    "revision": "7fb3224cde759677b98b",
+    "url": "/static/js/runtime-main.55fec6b0.js"
+  }
+]);
