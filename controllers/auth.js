53fe78fb8087e325c083312d537a537e58b0a815
facudelucia <facundodeluciajudo@gmail.com>
2020-07-30 09:43:51 -0300

    revalidatetoken updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,6 +91,8 @@ const revalidateToken = async (req, res = response) => {
     const token = await generateJWT(uid, name)
     res.json({
         ok: true,
+        uid,
+        name,
         token
     })
 }
